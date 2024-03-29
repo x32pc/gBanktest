40c982afab5de0eeca4465b855157c03a5719efd
x32pc <80939238+x32pc@users.noreply.github.com>
2024-03-29 11:14:49 +0100

    Commit

--- src/main/java/com/x32pc/github/gbanktest/GBanktest.java
+++ src/main/java/com/x32pc/github/gbanktest/GBanktest.java
@@ -1,2 +1,24 @@
-package com.x32pc.github.gbanktest;public class GBanktest {
+package com.x32pc.github.gbanktest;
+
+import com.x32pc.github.GBank;
+import com.x32pc.github.api.API;
+import com.x32pc.github.api.APIImplementation;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GBanktest extends JavaPlugin {
+    public API api;
+
+    public void onEnable() {
+        api = this.getServer().getServicesManager().load(API.class);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage(String.valueOf(api.getAmountCurrency(Bukkit.getOfflinePlayer(args[0]).getUniqueId().toString(), args[1])));
+        return true;
+    }
 }
